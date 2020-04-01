673684b7607546126a9db414b87c6ffe6447c9b0
Federico Ladereche <federicobrasilcuritiba@gmail.com>
2020-04-01 17:04:32 -0300

    Este commit tiene cambios pertinentes para la segunda para la aplicación de la Sonda EXO de laguna del Sauce.

---
+++ src/beans/SondeDataJSONObject.java
@@ -0,0 +1,46 @@
+package beans;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class SondeDataJSONObject extends JSONObject{
+
+	public SondeDataJSONObject(String idContrato, Date dateTime, Object data) {
+		super(idContrato, dateTime, data);
+	}
+	
+	protected void fillDataFromSondeData(Object data) {
+		SondeData sondeData = (SondeData)data;
+		
+		this.data = new ArrayList();
+		ArrayList valueFieldBattery = new ArrayList<>();
+		ArrayList valueFieldCable = new ArrayList<>();
+		ArrayList valueFieldPressure = new ArrayList<>();
+		
+		valueFieldBattery.add("6");
+		valueFieldBattery.add("bp");
+		valueFieldBattery.add(sondeData.getBatteryPower());		
+		valueFieldBattery.add("1");		
+		valueFieldBattery.add("1");
+		
+		this.data.add(valueFieldBattery);
+		
+		valueFieldCable.add("6");
+		valueFieldCable.add("cp");
+		valueFieldCable.add(sondeData.getCablePower());		
+		valueFieldCable.add("1");		
+		valueFieldCable.add("1");
+		
+		this.data.add(valueFieldCable);
+		
+		valueFieldPressure.add("6");
+		valueFieldPressure.add("pp");
+		valueFieldPressure.add(sondeData.getPressure());		
+		valueFieldPressure.add("1");		
+		valueFieldPressure.add("1");
+		
+		this.data.add(valueFieldPressure);
+		
+	}
+	
+}
